b563c7f413714cbd246ea236affbc6b630a717be
Jaya Surya <chitrasivakumar280@gmail.com>
2026-08-10 10:11:06 +0530

    init

---
+++ .next/dev/server/chunks/ssr/src_app_not-found_tsx_1863-sq._.js
@@ -0,0 +1,11 @@
+module.exports = [
+"[project]/src/app/not-found.tsx [app-rsc] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+"[project]/src/app/not-found.tsx [app-rsc] (ecmascript, Next.js Server Component)", (function(__turbopack_context__){
+
+__turbopack_context__.n(__turbopack_context__.i("[project]/src/app/not-found.tsx [app-rsc] (ecmascript)"));
+}),
+];
+
+//# sourceMappingURL=src_app_not-found_tsx_1863-sq._.js.map
